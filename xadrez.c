81a1f8dd0304b27981bf832c53d3ef0169136117
Renan Silva <101990719+renyzeraa@users.noreply.github.com>
2025-03-28 00:46:37 +0000

    feat: 🏅 Nível Aventureiro

--- xadrez.c
+++ xadrez.c
@@ -2,29 +2,52 @@
 
 int main()
 {
-  printf("Movimento da Torre:\n");
-  for (int i = 0; i < 5; i++)
-  {
-    printf("Direita\n");
-  }
-  printf("\n");
+    // Movimento da Torre
+    printf("Movimento da Torre:\n");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Direita\n");
+    }
+    printf("\n");
 
-  printf("Movimento do Bispo:\n");
-  int j = 0;
-  while (j < 5)
-  {
-    printf("Cima, Direita\n");
-    j++;
-  }
-  printf("\n");
+    // Movimento do Bispo
+    printf("Movimento do Bispo:\n");
+    int j = 0;
+    while (j < 5)
+    {
+        printf("Cima, Direita\n");
+        j++;
+    }
+    printf("\n");
 
-  printf("Movimento da Rainha:\n");
-  int k = 0;
-  do
-  {
-    printf("Esquerda\n");
-    k++;
-  } while (k < 8);
+    // Movimento da Rainha
+    printf("Movimento da Rainha:\n");
+    int k = 0;
+    do
+    {
+        printf("Esquerda\n");
+        k++;
+    } while (k < 8);
+    printf("\n");
 
-  return 0;
+    // Movimento do Cavalo
+    printf("Movimento do Cavalo:\n\n");
+    
+    // Definição das constantes de movimento
+    int movimentos_baixo = 2;
+    int movimentos_esquerda = 1;
+
+    // Loop aninhado para mover o Cavalo
+    for (int i = 0; i < movimentos_baixo; i++)
+    {
+        printf("Baixo\n");
+        int l = 0;
+        while (l < movimentos_esquerda && i == movimentos_baixo - 1)
+        {
+            printf("Esquerda\n");
+            l++;
+        }
+    }
+    
+    return 0;
 }
